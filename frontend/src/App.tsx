32e39f869b9326743c18a86db1511a8f23eddbd2
lerte smith <lerte@qq.com>
2024-04-09 23:37:55 +0800

    :tada:init repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-white">
+      <h1>Hello Zroker</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+};
