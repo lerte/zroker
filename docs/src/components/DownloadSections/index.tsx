60778f69684593db5d0fcb2fc269b1778215cea0
lerte smith <lerte@qq.com>
2024-04-16 16:42:01 +0800

    update download url

--- docs/src/components/DownloadSections/index.tsx
+++ docs/src/components/DownloadSections/index.tsx
@@ -1,20 +1,42 @@
 import styles from "./styles.module.css";
 
 export default function DownloadSections(): JSX.Element {
+  const version = "v0.0.1";
+  const windowUrl = `https://github.com/lerte/zroker/releases/download/${version}/Wails.Build.Windows.Zroker.zip`;
+  const macosUrl = `https://github.com/lerte/zroker/releases/download/${version}/Wails.Build.macOS.Zroker.zip`;
+  const linuxUrl = `https://github.com/lerte/zroker/releases/download/${version}/Wails.Build.Linux.Zroker.zip`;
+
   return (
     <section className={styles.downloads}>
       <div className="container">
         <div className="row">
           <div className="col col--4 text--center">
-            <button className="button button--lg button--success">
-              Windows
-            </button>
+            <a
+              download
+              href={windowUrl}
+            >
+              <button className="button button--lg button--success">
+                Windows
+              </button>
+            </a>
           </div>
           <div className="col col--4 text--center">
-            <button className="button button--lg button--info">macOS</button>
+            <a
+              download
+              href={macosUrl}
+            >
+              <button className="button button--lg button--info">macOS</button>
+            </a>
           </div>
           <div className="col col--4 text--center">
-            <button className="button button--lg button--warning">Linux</button>
+            <a
+              download
+              href={linuxUrl}
+            >
+              <button className="button button--lg button--warning">
+                Linux
+              </button>
+            </a>
           </div>
         </div>
       </div>
