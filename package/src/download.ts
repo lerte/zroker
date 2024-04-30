36d90631a4c8792a54a72d8ff23da0a24c921f1f
漆学龙 <qixuelong@ztoky.cn>
2024-04-30 13:56:01 +0800

    fix: update download.ts

--- package/src/download.ts
+++ package/src/download.ts
@@ -1,37 +1,28 @@
 import fs from "fs";
 import path from "path";
-import https from "https";
 import cliProgress from "cli-progress";
+import progress from "fetch-progress";
+import {Buffer} from "buffer";
 
-export default function (url: string) {
+export default async function (url: string) {
   const filename = path.basename(url);
-  if (fs.existsSync(filename)) {
-    return filename;
+  if (!fs.existsSync(filename)) {
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    const response = await fetch(url);
+    let started = false;
+    const blob = await progress({
+      onProgress(progress) {
+        if (!started) {
+          bar.start(progress.total, 0);
+          started = true;
+        }
+        bar.update(progress.transferred);
+        if (progress.transferred === progress.total) {
+          bar.stop();
+        }
+      },
+    })(response);
+    fs.writeFileSync(filename, Buffer.from(await blob.arrayBuffer()));
   }
-
-  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  let body = "";
-  let cur = 0;
-
-  return new Promise<string>((resolve, _reject) => {
-    https.get(`${url}?${Date.now()}`, (res) => {
-      const fileStream = fs.createWriteStream(filename);
-      res.pipe(fileStream);
-
-      const total = parseInt(res.headers["content-length"]!, 10);
-      bar.start(total, 0);
-
-      res.on("data", (chunk) => {
-        body += chunk;
-        cur += chunk.length;
-        bar.update(cur);
-      });
-
-      fileStream.on("finish", () => {
-        fileStream.close();
-        bar.stop();
-        resolve(filename);
-      });
-    });
-  });
+  return filename;
 }
